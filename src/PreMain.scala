f0241671d15aa7c86dea11043244bd42f04bef43
Steve Waldman <swaldman@mchange.com>
2024-01-29 19:50:06 +0000

    Add a customizer for tech mastoannouncements.

--- src/PreMain.scala
+++ src/PreMain.scala
@@ -1,9 +1,18 @@
-import com.mchange.feedletter.{UserUntemplates,Main}
+import com.mchange.feedletter.*
 import com.mchange.feedletter.style.{AllUntemplates,StyleMain}
+
+import com.mchange.feedletter.style.Customizer
+
+val TechMastonotifyCustomizer : Customizer.MastoAnnouncement =
+  ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, withinTypeId : String, feedUrl : FeedUrl, content : ItemContent ) =>
+    content.link.map: url =>
+      val title = content.title.getOrElse("(untitled)")
+      s"[tech notebook] ${title} ${url}"
 
 object PreMain:
   def main( args : Array[String] ) : Unit =
     AllUntemplates.add( UserUntemplates )
+    Customizer.MastoAnnouncement.register("tech-mastonotify", TechMastonotifyCustomizer)
     val styleExec =
       sys.env.get("FEEDLETTER_STYLE") match
         case Some( s ) => s.toBoolean
