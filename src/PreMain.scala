27783112e064bd5981ec44553c89caf0245c78ab
Steve Waldman <swaldman@mchange.com>
2024-01-29 23:50:32 +0000

    Add customizers for mastodon announcements for drafts and interfluidity-main.

--- src/PreMain.scala
+++ src/PreMain.scala
@@ -8,11 +8,24 @@ val TechMastonotifyCustomizer : Customizer.MastoAnnouncement =
     content.link.map: url =>
       val title = content.title.getOrElse("(untitled)")
       s"[tech notebook] ${title} ${url}"
+val MainMastonotifyCustomizer : Customizer.MastoAnnouncement =
+  ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, withinTypeId : String, feedUrl : FeedUrl, content : ItemContent ) =>
+    content.link.map: url =>
+      val title = content.title.getOrElse("(untitled)")
+      s"[New Post] ${title} ${url}"
+val DraftsMastonotifyCustomizer : Customizer.MastoAnnouncement =
+  ( subscribableName : SubscribableName, subscriptionManager : SubscriptionManager, withinTypeId : String, feedUrl : FeedUrl, content : ItemContent ) =>
+    content.link.map: url =>
+      val title = content.title.getOrElse("(untitled)")
+      s"[new draft post] ${title} ${url}"
+      
 
 object PreMain:
   def main( args : Array[String] ) : Unit =
     AllUntemplates.add( UserUntemplates )
     Customizer.MastoAnnouncement.register("tech-mastonotify", TechMastonotifyCustomizer)
+    Customizer.MastoAnnouncement.register("interfluidity-main-mastonotify", MainMastonotifyCustomizer)
+    Customizer.MastoAnnouncement.register("drafts-mastonotify", DraftsMastonotifyCustomizer)
     val styleExec =
       sys.env.get("FEEDLETTER_STYLE") match
         case Some( s ) => s.toBoolean
